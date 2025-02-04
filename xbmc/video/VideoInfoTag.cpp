78andyp/scrape_disc

Export bluray:// items
Ensures bluray:// paths are resolved correctly for NFO and art location when exporting as separate files.
Ensures the bluray:// (relative) path is saved otherwise the restored item's file will point at back at the base file (.ISO or index.bdmv).

xbmc/video/VideoInfoTag.cpp
@@ -9,12 +9,14 @@
 #include "VideoInfoTag.h"
 
 #include "ServiceBroker.h"
+#include "URL.h"
 #include "guilib/LocalizeStrings.h"
 #include "imagefiles/ImageFileURL.h"
 #include "settings/AdvancedSettings.h"
 #include "settings/SettingsComponent.h"
 #include "utils/Archive.h"
 #include "utils/StringUtils.h"
+#include "utils/URIUtils.h"
 #include "utils/Variant.h"
 #include "utils/XMLUtils.h"
 #include "utils/log.h"
@@ -194,6 +196,11 @@ bool CVideoInfoTag::Save(TiXmlNode *node, const std::string &tag, bool savePathI
     XMLUtils::SetString(movie, "path", m_strPath);
     XMLUtils::SetString(movie, "filenameandpath", m_strFileNameAndPath);
     XMLUtils::SetString(movie, "basepath", m_basePath);
+  }
+  else if (URIUtils::IsBlurayPath(m_strFileNameAndPath))
+  {
+    const CURL url{m_strFileNameAndPath};
+    XMLUtils::SetString(movie, "relativefilename", url.GetFileName());
   }
   if (!m_strEpisodeGuide.empty())
   {

builder08/xbmc

xbmc/video/VideoInfoTag.cpp
@@ -9,12 +9,14 @@
 #include "VideoInfoTag.h"
 
 #include "ServiceBroker.h"
+#include "URL.h"
 #include "guilib/LocalizeStrings.h"
 #include "imagefiles/ImageFileURL.h"
 #include "settings/AdvancedSettings.h"
 #include "settings/SettingsComponent.h"
 #include "utils/Archive.h"
 #include "utils/StringUtils.h"
+#include "utils/URIUtils.h"
 #include "utils/Variant.h"
 #include "utils/XMLUtils.h"
 #include "utils/log.h"
@@ -194,6 +196,11 @@ bool CVideoInfoTag::Save(TiXmlNode *node, const std::string &tag, bool savePathI
     XMLUtils::SetString(movie, "path", m_strPath);
     XMLUtils::SetString(movie, "filenameandpath", m_strFileNameAndPath);
     XMLUtils::SetString(movie, "basepath", m_basePath);
+  }
+  else if (URIUtils::IsBlurayPath(m_strFileNameAndPath))
+  {
+    const CURL url{m_strFileNameAndPath};
+    XMLUtils::SetString(movie, "file", url.GetFileName());
   }
   if (!m_strEpisodeGuide.empty())
   {